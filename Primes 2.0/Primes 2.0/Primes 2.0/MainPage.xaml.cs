50101e487fc89395a6e1e13f2b2e7c7924cfe5ad
RadolinaMaria <Radolinaandreeva18@gmail.com>
2023-02-18 18:15:51 +0200

    Primes 2.0

---
+++ Primes 2.0/Primes 2.0/Primes 2.0/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace Primes_2._0
+{
+    /// <summary>
+    /// Business Logic (BL): Primes 2.0
+    /// </summary>
+    public partial class MainPage : ContentPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        // Button Click Event Handler
+        void OnButtonClicked(object sender, EventArgs args)
+        {
+            List<int> primes = new List<int>();
+            int limit = int.Parse(this.boxLimit.Text);
+            for (int k = 2; k < limit; k++)
+            {
+                bool prime = true;
+                for (int j = 2; j < k; j++) if (k % j == 0) prime = false;
+                if (prime) primes.Add(k);
+            }
+            this.boxNumbers.ItemsSource = primes;
+        }
+    }
+}
